4ffc5663ccee75af3728d2a8ff1c06f76b443eae
eduardozamit <zamit.eduardo@gmail.com>
2024-03-13 15:45:54 -0300

    First commit

---
+++ src/main/java/com/github/plataformadodaleAPI/PlataformaDoDaleApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.plataformadodaleAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDoDaleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaDoDaleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/plataformadodaleAPI/PlataformaDoDaleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.plataformadodaleAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaDoDaleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
