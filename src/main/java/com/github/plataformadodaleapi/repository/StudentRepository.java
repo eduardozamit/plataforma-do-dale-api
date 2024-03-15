f992a0842c231c48b1fcf28a9f8148de2edcbbec
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-15 20:12:02 -0300

    feat(repository): adiciona query de buscar estudantes com as competencias pelo id

--- src/main/java/com/github/plataformadodaleapi/repository/StudentRepository.java
+++ src/main/java/com/github/plataformadodaleapi/repository/StudentRepository.java
@@ -1,7 +1,6 @@
 package com.github.plataformadodaleapi.repository;
 
 import com.github.plataformadodaleapi.dto.StudentDTOQuery;
-import com.github.plataformadodaleapi.dto.response.StudentCompetenceResponse;
 import com.github.plataformadodaleapi.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
@@ -29,6 +29,6 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     @Query("SELECT s FROM Student s JOIN FETCH s.competences")
     List<Student> listStudentsCompetences();
 
-    @Query("SELECT new com.github.plataformadodaleapi.dto.response.StudentCompetenceResponse(s, c.description) FROM Student s JOIN s.competences c WHERE s.id = :id")
-    StudentCompetenceResponse listStudentCompetenceById(@Param("id") long id);
+    @Query("SELECT s FROM Student s JOIN FETCH s.competences WHERE s.id = :student_id")
+    Optional<Student> listStudentCompetenceById(@Param("student_id") long studentId);
 }
