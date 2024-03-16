b721835e8a56692bd9f22145ae75fe6263933299
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-16 15:07:38 -0300

    feat(RecruiterService): adiciona metodos GETs de Recruiter, DELETE, READ, POST e metodo de favoritar e desfavoritar Students

--- src/main/java/com/github/plataformadodaleapi/service/RecruiterService.java
+++ src/main/java/com/github/plataformadodaleapi/service/RecruiterService.java
@@ -2,7 +2,9 @@ package com.github.plataformadodaleapi.service;
 
 import com.github.plataformadodaleapi.model.recruiter.Recruiter;
 import com.github.plataformadodaleapi.model.recruiter.dto.request.RecruiterRequestDTO;
+import com.github.plataformadodaleapi.model.student.Student;
 import com.github.plataformadodaleapi.repository.RecruiterRepository;
+import com.github.plataformadodaleapi.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +15,12 @@ import java.util.Optional;
 public class RecruiterService {
 
     private final RecruiterRepository recruiterRepository;
+    private final StudentRepository studentRepository;
 
     @Autowired
-    public RecruiterService(RecruiterRepository recruiterRepository) {
+    public RecruiterService(RecruiterRepository recruiterRepository, StudentRepository studentRepository) {
         this.recruiterRepository = recruiterRepository;
+        this.studentRepository = studentRepository;
     }
 
     public List<Recruiter> getAllRecruiters() {
@@ -43,5 +47,31 @@ public class RecruiterService {
 
     public void deleteRecruiter(Long id) {
         recruiterRepository.deleteById(id);
+    }
+
+    public List<Student> getFavoriteStudentsById(Long id) {
+        Optional<Recruiter> recruiterFounded = recruiterRepository.findById(id);
+        return recruiterFounded.map(Recruiter::getFavoriteStudents).orElse(null);
+    }
+
+    public Recruiter favoriteStudentById(Long recruiterId, Long studentId) {
+        Optional<Recruiter> recruiterOptional = recruiterRepository.findById(recruiterId);
+        if (recruiterOptional.isPresent()) {
+            Optional<Student> studentOptional = studentRepository.findById(studentId);
+            if (studentOptional.isPresent()) {
+                recruiterOptional.get().getFavoriteStudents().add(studentOptional.get());
+                return recruiterRepository.save(recruiterOptional.get());
+            }
+        }
+        return null;
+    }
+
+    public Recruiter disfavorStudentById(Long recruiterId, Long studentId) {
+        Optional<Recruiter> recruiterFounded = recruiterRepository.findById(recruiterId);
+        if (recruiterFounded.isPresent()) {
+            recruiterFounded.get().getFavoriteStudents().removeIf(student -> student.getId() == studentId);
+            return recruiterRepository.save(recruiterFounded.get());
+        }
+        return null;
     }
 }
