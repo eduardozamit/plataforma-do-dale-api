f636a165dbc8caac07c55d84bf3600f0dca3f671
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-16 03:26:49 -0300

    feat(StudentController): adiciona metodo GET com todos filtros e reordena os metodos

--- src/main/java/com/github/plataformadodaleapi/controller/StudentController.java
+++ src/main/java/com/github/plataformadodaleapi/controller/StudentController.java
@@ -2,9 +2,10 @@ package com.github.plataformadodaleapi.controller;
 
 import com.github.plataformadodaleapi.dto.request.StudentRequestDTO;
 import com.github.plataformadodaleapi.dto.response.StudentResponse;
-import com.github.plataformadodaleapi.entity.GCTrail;
 import com.github.plataformadodaleapi.entity.Student;
+import com.github.plataformadodaleapi.repository.StudentFilterParam;
 import com.github.plataformadodaleapi.service.StudentService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,38 +21,48 @@ public class StudentController {
         this.service = service;
     }
 
-    @GetMapping
-    public List<StudentResponse> findAllStudents() {
-        return service.findAllStudents();
-    }
-
-    @GetMapping("/with-competence/{id}")
-    public ResponseEntity<Student> findStudentWithCompetenceById(@PathVariable long id) {
-        return service.listStudentWithCompetenceById(id).isPresent()
-                ? ResponseEntity.ok(service.listStudentWithCompetenceById(id).get())
-                : ResponseEntity.badRequest().build();
-    }
-
     @PostMapping("/salvar")
-    public Student saveStudent(@RequestBody StudentRequestDTO studentRequestDTO) {
-        return service.saveStudent(studentRequestDTO);
-    }
-
-    @PostMapping("/adicionar-competencia")
-    public Student addCompetence(@RequestParam("student") String studentId, @RequestParam("competence") String idCompetence) {
-        return service.addCompetenceToStudent(studentId, idCompetence);
+    public ResponseEntity<Student> saveStudent(@RequestBody StudentRequestDTO studentRequestDTO) {
+        Student student = service.saveStudent(studentRequestDTO);
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
     @PostMapping("/adicionar-competencia/mais-de-uma")
-    public ResponseEntity<Student> addCompetences(@RequestParam("student") String studentId, @RequestParam("competence") List<String> idsCompetences) {
-        Optional<Student> student = service.addManyCompetencesToStudent(studentId, idsCompetences);
-        return student.isPresent() ? ResponseEntity.ok(student.get()) : ResponseEntity.badRequest().build();
+    public ResponseEntity<Student> addCompetences(@RequestParam("student") Long studentId,
+                                                  @RequestParam("competence") List<Long> idsCompetences) {
+        Student student = service.addManyCompetencesToStudent(studentId, idsCompetences);
+        return student != null ? ResponseEntity.ok(student) : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @PostMapping("/adicionar-competencia")
+    public ResponseEntity<Student> addCompetence(@RequestParam("student") Long studentId,
+                                                 @RequestParam("competence") Long idCompetence) {
+        Student student = service.addCompetenceToStudent(studentId, idCompetence);
+        return student != null ? ResponseEntity.ok(student) : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @GetMapping("/sem-competencias")
+    public ResponseEntity<List<StudentResponse>> findAllStudents() {
+        List<StudentResponse> students = service.getAllStudents();
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getStudentsWithCompetences() {
+        List<Student> students = service.getAllStudentsWithCompetence();
+        return new ResponseEntity<>(students, HttpStatus.OK);
     }
 
     @GetMapping("/filtro")
-    public List<Student> findStudentsByFilter(@RequestParam(required = false, name = "name") String name,
-                                              @RequestParam(required = false, name = "age") Integer age,
-                                              @RequestParam(required = false, name = "trail") GCTrail gcTrail) {
-        return service.listStudentsByFilter(name, age, gcTrail);
+    public ResponseEntity<List<Student>> getWithFilter(@RequestBody StudentFilterParam params) {
+        List<Student> students = service.getAllByFilter(params);
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
+
+    @GetMapping("/with-competence/{id}")
+    public ResponseEntity<Student> findStudentWithCompetenceById(@PathVariable Long id) {
+        Optional<Student> studentOptional = service.getStudentWithCompetenceById(id);
+        return studentOptional.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 }
