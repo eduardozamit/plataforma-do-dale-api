4a4e6e3d342e3feb4e0a4a42abc7a491774db7c0
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-16 03:24:46 -0300

    feat(StudentService): troca posicao de metodos e adiciona GET com todos filtros possiveis

--- src/main/java/com/github/plataformadodaleapi/service/StudentService.java
+++ src/main/java/com/github/plataformadodaleapi/service/StudentService.java
@@ -3,10 +3,9 @@ package com.github.plataformadodaleapi.service;
 import com.github.plataformadodaleapi.dto.request.StudentRequestDTO;
 import com.github.plataformadodaleapi.dto.response.StudentResponse;
 import com.github.plataformadodaleapi.entity.Competence;
-import com.github.plataformadodaleapi.entity.GCTrail;
 import com.github.plataformadodaleapi.entity.Student;
 import com.github.plataformadodaleapi.repository.CompetenceRepository;
-import com.github.plataformadodaleapi.repository.StudentCustomRepository;
+import com.github.plataformadodaleapi.repository.StudentFilterParam;
 import com.github.plataformadodaleapi.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
@@ -14,17 +13,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-
 @Service
 public class StudentService {
     private StudentRepository studentRepository;
     private CompetenceRepository competenceRepository;
-    private StudentCustomRepository studentCustomRepository;
 
-    public StudentService(StudentRepository studentRepository, CompetenceRepository competenceRepository, StudentCustomRepository studentCustomRepository) {
+    public StudentService(StudentRepository studentRepository, CompetenceRepository competenceRepository) {
         this.studentRepository = studentRepository;
         this.competenceRepository = competenceRepository;
-        this.studentCustomRepository = studentCustomRepository;
     }
 
     public Student saveStudent(StudentRequestDTO studentRequestDTO) {
@@ -32,47 +28,49 @@ public class StudentService {
         return studentRepository.save(student);
     }
 
-    public List<StudentResponse> findAllStudents() {
-        List<Student> studentsList = studentRepository.findAll();
-        List<StudentResponse> students = new ArrayList<>();
-        for (Student studentIndex : studentsList) {
-            StudentResponse student = new StudentResponse(studentIndex);
-            students.add(student);
-        }
-        return students;
-    }
-
-    public Student addCompetenceToStudent(String studentId, String idCompetence) {
-        Optional<Student> student = studentRepository.findById(Long.parseLong(studentId));
-        if (student.isPresent()) {
-            Optional<Competence> competenceOptional = competenceRepository.findById(Long.parseLong(idCompetence));
-            competenceOptional.ifPresent(competence -> student.get().getCompetences().add(competence));
-            return studentRepository.save(student.get());
+    public Student addManyCompetencesToStudent(Long studentId, List<Long> listOfCompetences) {
+        Optional<Student> studentOptional = studentRepository.findById(studentId);
+        if (studentOptional.isPresent()) {
+            List<Competence> competences = new ArrayList<>();
+            for (Long id : listOfCompetences) {
+                Optional<Competence> competenceOptional = competenceRepository.findById(id);
+                competenceOptional.ifPresent(competences::add);
+            }
+            studentOptional.get().getCompetences().addAll(competences);
+            return studentRepository.save(studentOptional.get());
         }
         return null;
     }
 
-    public Optional<Student> addManyCompetencesToStudent(String studentId, List<String> listOfCompetences) {
-        Optional<Student> student = studentRepository.findById(Long.parseLong(studentId));
-        if (student.isPresent()) {
-            List<Competence> competences = new ArrayList<>();
-            for (String id : listOfCompetences) {
-                Optional<Competence> competence = competenceRepository.findById(Long.parseLong(id));
-                if (competence.isPresent()) {
-                    competences.add(competence.get());
-                }
-            }
-            student.get().getCompetences().addAll(competences);
-            return student;
+    public Student addCompetenceToStudent(Long studentId, Long idCompetence) {
+        Optional<Student> studentOptional = studentRepository.findById(studentId);
+        if (studentOptional.isPresent()) {
+            Optional<Competence> competenceOptional = competenceRepository.findById(idCompetence);
+            competenceOptional.ifPresent(competence -> studentOptional.get().getCompetences().add(competence));
+            return studentRepository.save(studentOptional.get());
         }
-        return Optional.empty();
+        return null;
     }
 
-    public Optional<Student> listStudentWithCompetenceById(long studentId) {
-        return studentRepository.listStudentCompetenceById(studentId);
+    public List<StudentResponse> getAllStudents() {
+        List<Student> studentsList = studentRepository.findAll();
+        List<StudentResponse> students = new ArrayList<>();
+        for (Student studentIndex : studentsList) {
+            StudentResponse studentResponse = new StudentResponse(studentIndex);
+            students.add(studentResponse);
+        }
+        return students;
     }
 
-    public List<Student> listStudentsByFilter(String name, Integer age, GCTrail gcTrail) {
-        return studentCustomRepository.findCustom(name, age, gcTrail);
+    public Optional<Student> getStudentWithCompetenceById(long studentId) {
+        return studentRepository.getStudentCompetenceById(studentId);
+    }
+
+    public List<Student> getAllStudentsWithCompetence() {
+        return studentRepository.getAllStudentsCompetences();
+    }
+
+    public List<Student> getAllByFilter(StudentFilterParam params) {
+        return this.studentRepository.getWithFilter(params);
     }
 }
