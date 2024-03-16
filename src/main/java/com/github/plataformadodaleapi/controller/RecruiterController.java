633db55608ec68e6a1b849bb606a1fffb8b6a371
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-16 05:25:25 -0300

    feat(RecruiterController): adiciona CRUD de Recruiter

---
+++ src/main/java/com/github/plataformadodaleapi/controller/RecruiterController.java
@@ -0,0 +1,59 @@
+package com.github.plataformadodaleapi.controller;
+
+import com.github.plataformadodaleapi.model.recruiter.Recruiter;
+import com.github.plataformadodaleapi.model.recruiter.dto.request.RecruiterRequestDTO;
+import com.github.plataformadodaleapi.service.RecruiterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/recruiters")
+public class RecruiterController {
+
+    private final RecruiterService recruiterService;
+
+    @Autowired
+    public RecruiterController(RecruiterService recruiterService) {
+        this.recruiterService = recruiterService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Recruiter>> getAllRecruiters() {
+        List<Recruiter> recruiters = recruiterService.getAllRecruiters();
+        return ResponseEntity.ok(recruiters);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Recruiter> getRecruiterById(@PathVariable Long id) {
+        Recruiter recruiter = recruiterService.getRecruiterById(id);
+        return recruiter != null
+                ? ResponseEntity.status(HttpStatus.OK).body(recruiter)
+                : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Recruiter> createRecruiter(@RequestBody RecruiterRequestDTO requestDto) {
+        Recruiter recruiter = recruiterService.createRecruiter(requestDto);
+        return recruiter != null
+                ? ResponseEntity.status(HttpStatus.CREATED).body(recruiter)
+                : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Recruiter> updateRecruiter(@PathVariable Long id, @RequestBody RecruiterRequestDTO requestDto) {
+        Recruiter recruiterFounded = recruiterService.updateRecruiter(id, requestDto);
+        return recruiterFounded != null
+                ? ResponseEntity.status(HttpStatus.OK).body(recruiterFounded)
+                : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteRecruiter(@PathVariable Long id) {
+        recruiterService.deleteRecruiter(id);
+        return ResponseEntity.noContent().build();
+    }
+}
