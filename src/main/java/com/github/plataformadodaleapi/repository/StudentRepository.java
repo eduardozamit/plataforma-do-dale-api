3471cdf339135d3c7c13a3d4510aa45314dee944
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-15 21:08:03 -0300

    refactor(StudentRepository): remove metodo inutilizado

--- src/main/java/com/github/plataformadodaleapi/repository/StudentRepository.java
+++ src/main/java/com/github/plataformadodaleapi/repository/StudentRepository.java
@@ -1,6 +1,5 @@
 package com.github.plataformadodaleapi.repository;
 
-import com.github.plataformadodaleapi.dto.StudentDTOQuery;
 import com.github.plataformadodaleapi.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,20 +11,6 @@ import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-
-
-    @Query(value = "SELECT " +
-            "Student.id," +
-            " Student.age," +
-            " Student.educationLevel," +
-            " Student.gcTrail, " +
-            "Competence.description" +
-            " FROM Student " +
-            "JOIN student_competence ON Student.id = student_competence.student_id " +
-            "JOIN Competence ON student_competence.competence_id = competence.id " +
-            "WHERE student_competence.id = :id", nativeQuery = true)
-    List<StudentDTOQuery> findStudentCompetence(@Param("id") long id);
-
     @Query("SELECT s FROM Student s JOIN FETCH s.competences")
     List<Student> listStudentsCompetences();
 
