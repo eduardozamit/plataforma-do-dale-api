136a6158371847146d64613a230d52f40e26346a
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-17 23:08:38 -0300

    feat(StudentRepository): remove metodo nao utilizado

--- src/main/java/com/github/plataformadodaleapi/repository/StudentRepository.java
+++ src/main/java/com/github/plataformadodaleapi/repository/StudentRepository.java
@@ -2,13 +2,8 @@ package com.github.plataformadodaleapi.repository;
 
 import com.github.plataformadodaleapi.model.student.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long>, StudentRepositoryCustom {
-    @Query("SELECT s FROM Student s JOIN FETCH s.competences")
-    List<Student> getAllStudentsCompetences();
 }
