c1648a1e49ba3d20149196ecb1ff7004e4211728
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-19 00:49:39 -0300

    feat(target): atualiza .jar

--- src/main/java/com/github/plataformadodaleapi/repository/HardSkillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.plataformadodaleapi.repository;
-
-import com.github.plataformadodaleapi.model.skills.HardSkill;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HardSkillRepository extends JpaRepository<HardSkill, Long> {
-}
