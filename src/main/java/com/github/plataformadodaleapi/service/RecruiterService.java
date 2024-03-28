453efacbda246aa13a767692bb7e4c111cd284aa
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-28 05:57:59 -0300

    feat(RecruiterService): adiciona logica para permitir apenas o proprio recrutador realizar alteracoes no perfil

--- src/main/java/com/github/plataformadodaleapi/service/RecruiterService.java
+++ src/main/java/com/github/plataformadodaleapi/service/RecruiterService.java
@@ -1,11 +1,16 @@
 package com.github.plataformadodaleapi.service;
 
-import com.github.plataformadodaleapi.model.recruiter.Recruiter;
-import com.github.plataformadodaleapi.model.recruiter.RecruiterRequestDTO;
+import com.github.plataformadodaleapi.model.recruiter.RecruiterDetalingData;
+import com.github.plataformadodaleapi.model.recruiter.RecruiterModel;
+import com.github.plataformadodaleapi.model.recruiter.RegisterRecruiterDTO;
+import com.github.plataformadodaleapi.model.recruiter.UpdateRecruiterDTO;
 import com.github.plataformadodaleapi.model.student.Student;
 import com.github.plataformadodaleapi.repository.RecruiterRepository;
 import com.github.plataformadodaleapi.repository.StudentRepository;
+import com.github.plataformadodaleapi.security.SecurityFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,65 +18,78 @@ import java.util.Optional;
 
 @Service
 public class RecruiterService {
+    private RecruiterRepository recruiterRepository;
+    private StudentRepository studentRepository;
+    private PasswordEncoder passwordEncoder;
+    private TokenService tokenService;
+    private SecurityFilter securityFilter;
 
-    private final RecruiterRepository recruiterRepository;
-    private final StudentRepository studentRepository;
-
-    @Autowired
-    public RecruiterService(RecruiterRepository recruiterRepository, StudentRepository studentRepository) {
-        this.recruiterRepository = recruiterRepository;
-        this.studentRepository = studentRepository;
+    public RecruiterDetalingData createRecruiter(RegisterRecruiterDTO dto) {
+        RecruiterModel recruiterModel = new RecruiterModel(dto);
+        recruiterModel.setPassword(passwordEncoder.encode(dto.password()));
+        recruiterRepository.save(recruiterModel);
+        return new RecruiterDetalingData(recruiterModel);
     }
 
-    public List<Recruiter> getAllRecruiters() {
-        return recruiterRepository.findAll();
+    public RecruiterDetalingData updateRecruiter(UpdateRecruiterDTO updateDTO, HttpServletRequest request) {
+        Long id = recoverIdByToken(request);
+        RecruiterModel recruiterModel = recruiterRepository.getReferenceById(id);
+        recruiterModel.updateInformation(updateDTO);
+        return new RecruiterDetalingData(recruiterModel);
     }
 
-    public Recruiter getRecruiterById(Long id) {
-        Optional<Recruiter> recruiterFounded = recruiterRepository.findById(id);
-        return recruiterFounded.orElse(null);
+    public RecruiterDetalingData getRecruiterProfile(HttpServletRequest request) {
+        Long id = recoverIdByToken(request);
+        RecruiterModel recruiterModel = recruiterRepository.getReferenceById(id);
+        return new RecruiterDetalingData(recruiterModel);
     }
 
-    public Recruiter createRecruiter(RecruiterRequestDTO requestDTO) {
-        return recruiterRepository.save(new Recruiter(requestDTO));
-    }
-
-    public Recruiter updateRecruiter(Long id, RecruiterRequestDTO requestDTO) {
-        Optional<Recruiter> recruiterFounded = recruiterRepository.findById(id);
-        if (recruiterFounded.isPresent()) {
-            recruiterFounded.get().setName(requestDTO.name());
-            return recruiterRepository.save(recruiterFounded.get());
-        }
-        return null;
-    }
-
-    public void deleteRecruiter(Long id) {
+    public void deleteRecruiter(HttpServletRequest request) {
+        Long id = recoverIdByToken(request);
         recruiterRepository.deleteById(id);
     }
 
-    public List<Student> getFavoriteStudentsById(Long id) {
-        Optional<Recruiter> recruiterFounded = recruiterRepository.findById(id);
-        return recruiterFounded.map(Recruiter::getFavoriteStudents).orElse(null);
+    public List<Student> getFavoriteStudents(HttpServletRequest request) {
+        Long id = recoverIdByToken(request);
+        Optional<RecruiterModel> recruiterFounded = recruiterRepository.findById(id);
+        return recruiterFounded.map(RecruiterModel::getFavoriteStudents).orElse(null);
     }
 
-    public Recruiter favoriteStudentById(Long recruiterId, Long studentId) {
-        Optional<Recruiter> recruiterOptional = recruiterRepository.findById(recruiterId);
+    public RecruiterDetalingData favoriteStudent(HttpServletRequest request, Long studentId) {
+        Long recruiterId = recoverIdByToken(request);
+        Optional<RecruiterModel> recruiterOptional = recruiterRepository.findById(recruiterId);
         if (recruiterOptional.isPresent()) {
             Optional<Student> studentOptional = studentRepository.findById(studentId);
             if (studentOptional.isPresent()) {
                 recruiterOptional.get().getFavoriteStudents().add(studentOptional.get());
-                return recruiterRepository.save(recruiterOptional.get());
+                return new RecruiterDetalingData(recruiterRepository.save(recruiterOptional.get()));
             }
         }
         return null;
     }
 
-    public Recruiter disfavorStudentById(Long recruiterId, Long studentId) {
-        Optional<Recruiter> recruiterFounded = recruiterRepository.findById(recruiterId);
+    public RecruiterDetalingData disfavorStudent(HttpServletRequest request, Long studentId) {
+        Long recruiterId = recoverIdByToken(request);
+        Optional<RecruiterModel> recruiterFounded = recruiterRepository.findById(recruiterId);
         if (recruiterFounded.isPresent()) {
             recruiterFounded.get().getFavoriteStudents().removeIf(student -> student.getId() == studentId);
-            return recruiterRepository.save(recruiterFounded.get());
+            return new RecruiterDetalingData(recruiterRepository.save(recruiterFounded.get()));
         }
         return null;
+    }
+
+    public Long recoverIdByToken(HttpServletRequest request) {
+        String tokenJwt = securityFilter.recoverToken(request);
+
+        return tokenService.getIdRecruiter(tokenJwt);
+    }
+
+    @Autowired
+    public RecruiterService(RecruiterRepository recruiterRepository, StudentRepository studentRepository, PasswordEncoder passwordEncoder, TokenService tokenService, SecurityFilter securityFilter) {
+        this.recruiterRepository = recruiterRepository;
+        this.studentRepository = studentRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.tokenService = tokenService;
+        this.securityFilter = securityFilter;
     }
 }
