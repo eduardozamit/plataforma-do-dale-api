388e2e754df1c2d295ae83d781c4d8e3e6a48060
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-28 05:56:41 -0300

    feat(AuthenticationController): adiciona controller com endpoint de login e para verificar se ja ha um cadastro com email x

---
+++ src/main/java/com/github/plataformadodaleapi/controller/AuthenticationController.java
@@ -0,0 +1,51 @@
+package com.github.plataformadodaleapi.controller;
+
+
+import com.github.plataformadodaleapi.model.recruiter.AuthenticationRecruiterDTO;
+import com.github.plataformadodaleapi.model.recruiter.RecruiterModel;
+import com.github.plataformadodaleapi.repository.RecruiterRepository;
+import com.github.plataformadodaleapi.security.TokenJWTDTO;
+import com.github.plataformadodaleapi.service.TokenService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+    private AuthenticationManager authenticationManager;
+    private RecruiterRepository recruiterRepository;
+    private TokenService tokenService;
+
+    @PostMapping("/verification")
+    @Transactional
+    public ResponseEntity<Boolean> authentication(@RequestBody MultiValueMap<String, String> data) {
+        String email = data.getFirst("email");
+        boolean emailExists = recruiterRepository.existsByemail(email);
+        return ResponseEntity.ok(emailExists);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenJWTDTO> login(@RequestBody @Valid AuthenticationRecruiterDTO dto) {
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+        var tokenJWT = tokenService.generateToken((RecruiterModel) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new TokenJWTDTO(tokenJWT));
+    }
+
+    public AuthenticationController(AuthenticationManager authenticationManager, RecruiterRepository recruiterRepository, TokenService tokenService) {
+        this.authenticationManager = authenticationManager;
+        this.recruiterRepository = recruiterRepository;
+        this.tokenService = tokenService;
+    }
+}
