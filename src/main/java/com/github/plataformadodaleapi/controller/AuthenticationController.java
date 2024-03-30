0bf566a76d9bb4acf91d25cb47c84c2cdde4abeb
Eduardo Fettermann <eduardofettermann212@gmail.com>
2024-03-30 14:27:31 -0300

    refactor(AuthenticationController): troca request body da verificacao de email

--- src/main/java/com/github/plataformadodaleapi/controller/AuthenticationController.java
+++ src/main/java/com/github/plataformadodaleapi/controller/AuthenticationController.java
@@ -1,18 +1,17 @@
 package com.github.plataformadodaleapi.controller;
 
 
+import com.github.plataformadodaleapi.model.auth.EmailVerificationDTO;
 import com.github.plataformadodaleapi.model.recruiter.AuthenticationRecruiterDTO;
 import com.github.plataformadodaleapi.model.recruiter.RecruiterModel;
 import com.github.plataformadodaleapi.repository.RecruiterRepository;
 import com.github.plataformadodaleapi.security.TokenJWTDTO;
 import com.github.plataformadodaleapi.service.TokenService;
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,14 +26,14 @@ public class AuthenticationController {
 
     @PostMapping("/verification")
     @Transactional
-    public ResponseEntity<Boolean> authentication(@RequestBody MultiValueMap<String, String> data) {
-        String email = data.getFirst("email");
+    public ResponseEntity<Boolean> authentication(@RequestBody EmailVerificationDTO dto) {
+        String email = dto.email();
         boolean emailExists = recruiterRepository.existsByemail(email);
         return ResponseEntity.ok(emailExists);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<TokenJWTDTO> login(@RequestBody @Valid AuthenticationRecruiterDTO dto) {
+    public ResponseEntity<TokenJWTDTO> login(@RequestBody AuthenticationRecruiterDTO dto) {
         var authenticationToken = new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
         Authentication authentication = authenticationManager.authenticate(authenticationToken);
 
